89ec9307aa27b2c374c7c2532f8beaf4e8bc5b10
daniel-jp <danny198672@hotmail.com>
2023-11-02 00:51:19 +0100

    fix images2

--- src/components/homComponent/HomComponent.tsx
+++ src/components/homComponent/HomComponent.tsx
@@ -65,7 +65,7 @@ export default function homComponent() {
               alt={'Daniel'}
               fit={'cover'}
               align={'center'}
-              src={"./public/danny1.png"}
+              src={"../../../dist/danny1.png"}
               
               />
 
