45c015abe41d2c9d9b60990b968e15557a933331
daniel-jp <danny198672@hotmail.com>
2023-11-18 02:30:09 +0100

    project links added

--- src/props/PortfolioProps/PortfolioProps.ts
+++ src/props/PortfolioProps/PortfolioProps.ts
@@ -6,14 +6,14 @@ export default function PortfolioProps(){
   const portfolioProps =[
     { id: 1,
       titlec: 'FD-GROUP',
-      textc: 'created with React, Nodejs and MongoDB',
+      textc: 'Created with React + TypeScript + Vite + Chakra-ui',
       iconc: ArrowSquareOut ,
       imgc:"../../assets/cardImg/Card1.png",
       href:"https://www.fdgroup.company/"
     },
     { id: 2,
       titlec: 'ACHAHADA',
-      textc: 'created with React, Nodejs and MongoDB',
+      textc: 'Created with Html5 + CSS3 + javaScript',
       iconc: ArrowSquareOut,
       imgc:"../../assets/cardImg/Card2.jpeg",
       href:"https://achahada-ubt.vercel.app/"
@@ -21,21 +21,21 @@ export default function PortfolioProps(){
     ,
     { id: 3,
       titlec: 'HOUSE RENTAL',
-      textc: 'created with React, Nodejs and MongoDB',
+      textc: 'Created with React + TS, Chakra-Ui Nodejs and MongoDB',
       iconc:ArrowSquareOut,
       imgc:"../../assets/cardImg/Card3.png",
       href:"https://meulugar.vercel.app/"
     }, 
     { id: 4,
       titlec: 'SERIES & MOVIES',
-      textc: 'created with js + Vite, Html and Css,  ',
+      textc: 'Created with js + Vite + Html5 + Css3,  ',
       iconc:ArrowSquareOut,
       imgc:"../../assets/cardImg/Card4.jpg",  
       href:"https://daniel-jp.github.io/Links-Aggregator/"
     },
     { id: 5,
       titlec: 'CRUD',
-      textc: 'created with Angular, SpringBoot and Mysql',
+      textc: 'Created with Angular + SpringBoot + Mysql',
       iconc:ArrowSquareOut, 
       imgc:"../../assets/cardImg/Card5.png",
       href:"https://github.com/daniel-jp/angular-springBoot"
@@ -43,7 +43,7 @@ export default function PortfolioProps(){
     ,
     { id: 6,
       titlec: 'REST-API & DTO',
-      textc: 'created with SpringBoot, Mysql and POSTMAN',
+      textc: 'Created with SpringBoot + Mysql + POSTMAN',
       iconc:ArrowSquareOut,
       imgc:"../../assets/cardImg/cardApi.jpeg",
       href:"https://github.com/daniel-jp/SpringREST_API"
